4ee06253c1863c22ee496a367f767d8fd6014906
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2023-11-28 11:29:42 -0700

    initial commit: sting file done

---
+++ strings.js
@@ -0,0 +1,14 @@
+const hello = "Hello World!";
+let stringWithSingleQuotes = 'Single quotes work too';
+
+console.log (hello[0]);
+
+const ball = "ball";
+const templateString = `Foot${ball}`;
+
+console.log(templateString);
+
+let yourName = "steve";
+console.log(`Hi ${yourName}`);
+
+console.log("Hi, " + yourName + "!");
